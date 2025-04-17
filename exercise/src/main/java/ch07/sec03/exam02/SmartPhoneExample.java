15c175ce2cd969b6bf84da5e4249d04b94c24415
Jiho <rkdwlgh01@naver.com>
2025-04-17 16:29:51 +0900

    20250417 과제 - 기본 문제

---
+++ exercise/src/main/java/ch07/sec03/exam02/SmartPhoneExample.java
@@ -0,0 +1,10 @@
+package ch07.sec03.exam02;
+
+public class SmartPhoneExample {
+    public static void main(String[] args) {
+        SmartPhone smartPhone = new SmartPhone("SmartPhone", "Blue");
+
+        System.out.println("model: " + smartPhone.model);
+        System.out.println("color: " + smartPhone.color);
+    }
+}
